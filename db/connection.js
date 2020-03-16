5f5448c1c4c95816bc58fc1296843640196c5830
Sean Rooney <seandanielrooney@gmail.com>
2020-03-16 16:10:06 -0700

    reorganizing files

---
+++ db/connection.js
@@ -0,0 +1,29 @@
+const mysql = require("mysql");
+const util = require("util");
+const fs = require("fs");
+const path = require("path");
+
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    // database: "employees_db"
+  });
+
+  connection.connect((err)=> {
+    if (err) throw err
+    else console.log("connection successful!");
+    const createSchema = fs.readFileSync(path.join(__dirname, "./schema.sql"), "utf8");
+    console.log(createSchema);
+    connection.query(createSchema, (err, result) =>{
+      if (err) throw err
+      console.log(result);
+      
+    })
+  });
+  connection.query = util.promisify(connection.query);
+
+  module.exports = connection;
+
---
+++ index.js
@@ -0,0 +1,11 @@
+//create classes with functions to perform all needed actions here
+
+const inquirer = require('inquirer');
+require('console.table');
+
+inquirer.prompt([
+    {
+        
+    }
+
+])
